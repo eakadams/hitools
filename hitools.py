f1749cd53aa6a86c7bbd862850c993534a27f8b3
Betsey Adams <eakadams@gmail.com>
2021-01-05 14:47:45 +0100

    First commit'

---
+++ hitools.py
@@ -0,0 +1,16 @@
+#Useful HI tools
+
+from __future__ import print_function
+
+__author__ = "E.A.K. Adams"
+
+"""
+Collection of HI tools that I find useful
+"""
+
+import numpy as np
+import astropy.units as u
+
+def get_mhi_unres():
+    """
+    """
